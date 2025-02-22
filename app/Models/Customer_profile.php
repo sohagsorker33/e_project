b4d8193ce6ada99b5e4375940bd4173b806f88ae
asraful <sohagali09876543@gmail.com>
2025-02-22 21:08:17 +0600

    m-CustomerProfile

--- app/Models/Customer_profile.php
+++ app/Models/Customer_profile.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Customer_profile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'cus_name',
+        'cus_add',
+        'cus_city',
+        'cus_state',
+        'cus_postcode',
+        'cus_country',
+        'cus_phone',
+        'cus_fax',
+        'ship_name',
+        'ship_add',
+        'ship_city',
+        'ship_state',
+        'ship_postcode',
+        'ship_country',
+        'ship_phone',
+        'user_id'
+     
+
+    ];
 }
