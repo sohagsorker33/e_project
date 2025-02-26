c2c9d6913278951f12c5078be9f05283f057d6cf
asraful <sohagali09876543@gmail.com>
2025-02-26 18:32:57 +0600

    m-product_details

--- app/Models/Product_detail.php
+++ app/Models/Product_detail.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product_detail extends Model
 {
     use HasFactory;
+
+    function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
