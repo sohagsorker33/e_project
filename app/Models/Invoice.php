36085b12064c241f0d24e3645a9b3776cc68403a
asraful <sohagali09876543@gmail.com>
2025-02-27 18:55:55 +0600

    m-Invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'total',
+        'vat',
+        'payable',
+        'cus_detalis',
+        'ship_detalis',
+        'tran_id',
+        'val_id',
+        'delivery_status',
+        'payment_status',
+        'user_id'
+    ];
 }
