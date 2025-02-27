a19fe81f5f9dfff5e56fc17f6822bf27689f91f3
asraful <sohagali09876543@gmail.com>
2025-02-27 18:59:06 +0600

    m-ProductCart

--- app/Models/Product_cart.php
+++ app/Models/Product_cart.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product_cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'color',
+        'size',
+        'qty',
+        'price',
+        'product_id',
+        'user_id',
+    ];
+
+    function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
