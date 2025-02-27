92d195599cd70ab5a5ab57b34b228012faec2215
asraful <sohagali09876543@gmail.com>
2025-02-27 19:02:09 +0600

    m-ProductReview

--- app/Models/Product_review.php
+++ app/Models/Product_review.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product_review extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'rating',
+        'customer_id',
+        'product_id',
+    ];
+
+    function profile(){
+        return $this->belongsTo(Customer_profile::class);
+    }
 }
