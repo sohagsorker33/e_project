30b3b7f568a38dab2732c16de7699dd829e7efb1
asraful <sohagali09876543@gmail.com>
2025-02-27 18:51:03 +0600

    m-InvoiceProduct

--- app/Models/Invoice_product.php
+++ app/Models/Invoice_product.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice_product extends Model
 {
     use HasFactory;
+
+    protected $table = [
+        'qty',
+        'sele_price',
+        'invoice_id',
+        'product_id',
+        'user_id',
+    ];
+
+    function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
